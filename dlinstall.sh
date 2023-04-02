aae0dd23b40152ca0c45974b8f3c8a7cf659999a
JJRoyale <98667766+JJRoyale@users.noreply.github.com>
2023-04-02 11:48:50 +0200

    Update dlinstall.sh

--- dlinstall.sh
+++ dlinstall.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-curl -L https://raw.githubusercontent.com/JJRoyale/batocera.pro-installer/master/installer.sh -o installer.sh
+curl -sSL https://raw.githubusercontent.com/JJRoyale/batocera.pro-installer/master/installer.sh -o installer.sh
 chmod +x ./installer.sh
 ./installer.sh
