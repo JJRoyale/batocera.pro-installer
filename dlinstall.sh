c8bdadfff34cafa8ca7a7ea181063664bb1b09f8
JJRoyale <98667766+JJRoyale@users.noreply.github.com>
2023-04-02 11:48:07 +0200

    Fix files via upload

--- dlinstall.sh
+++ dlinstall.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 curl -L https://raw.githubusercontent.com/JJRoyale/batocera.pro-installer/master/installer.sh -o installer.sh
 chmod +x ./installer.sh
-bash ./installer.sh
+./installer.sh
